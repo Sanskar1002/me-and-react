d8a8c04e1e7fc447e8c02d60e397d5fe46ea121e
Sanskar Wankhede <sanskarw10@gmail.com>
2024-07-16 21:52:48 +0530

    use effect

--- 01basicsofReact/src/App.jsx
+++ 01basicsofReact/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div>Hello World </div>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("call useEffect");
+    document.title = `Increment (${value})`;
+  });
+
+  return (
+    <>
+      <h2>{value}</h2>
+      <button onClick={() => setValue(value + 1)}>Click me</button>
+    </>
+  );
 }
 
 export default App;
